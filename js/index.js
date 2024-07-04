3bb978cd96d5116ae616b57a97a9c8c5a5fa7e26
NayeraGad <naiera.gad30@gmail.com>
2024-07-04 18:47:38 +0300

    forth commit

--- js/index.js
+++ js/index.js
@@ -33,8 +33,7 @@ function login() {
   if (loggedInUser) {
     // Get user name and add it to local storage
     localStorage.setItem("loggedInUser", loggedInUser);
-    const baseUrl = window.location.origin;
-    const loginUrl = baseUrl + "/pages/main.html";
+    const loginUrl = "./pages/main.html";
     window.location.href = loginUrl;
   } else {
     alertMess.innerHTML = `<p class="text-danger p-0">Incorrect email or password</p>`;
